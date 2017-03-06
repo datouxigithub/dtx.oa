ccac16a8b92cc788f72d564745c97b19a582649a
datouxi <datouxi@localhost.localdomain>
2017-03-06 22:45:03 +0800

    init-rbac

---
+++ rbac/src/main/java/dtx/oa/rbac/model/NodeTree.java
@@ -0,0 +1,278 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtx.oa.rbac.model;
+
+import dtx.db.ControllerFactory;
+import dtx.rbac.controller.NodeController;
+import dtx.rbac.controller.RBACController;
+import dtx.rbac.controller.RoleNodeController;
+import dtx.rbac.controller.impl.DefaultNodeControllerImpl;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ *
+ * @author datouxi
+ */
+public class NodeTree {
+    private List<NodeTreeLeaf> rootLeaves=new ArrayList<>();
+    private int treeDepth=Integer.MIN_VALUE;
+    
+    public NodeTree(RBACController rbac){
+        init(rbac);
+    }
+    
+    public NodeTree(RBACController rbac,boolean status){
+        init(rbac, status);
+    }
+    
+    public NodeTree(List<Role> roles){
+        init(roles);
+    }
+    
+    public NodeTree(List<Role> roles,boolean status){
+        init(roles, status);
+    }
+    
+    public NodeTree(Node[] nodes){
+        init(nodes);
+    }
+    
+    public NodeTree(Node[] nodes,int nodeType){
+        init(nodes, nodeType);
+    }
+    
+    public NodeTree(Node[] nodes,boolean status){
+        init(nodes, status);
+    }
+    
+    public NodeTree(Node[] nodes,boolean status,int nodeType){
+        init(nodes, status, nodeType);
+    }
+    
+    private void init(RBACController rbac,boolean status){
+        if(!rbac.isLogin())return;
+        NodeController nc=ControllerFactory.getNodeController();
+        if(ControllerFactory.getUserController().isAdmin(rbac.getLoginInfo())){
+            Iterator<Node> iter=nc.getChilds(DefaultNodeControllerImpl.ROOTNODEID).iterator();
+            while(iter.hasNext()){
+                rootLeaves.add(new NodeTreeLeaf((Node)iter.next()));
+            }
+            checkRepeat();
+        }else{
+            init(ControllerFactory.getRoleUserController().getRoleByUser(rbac.getLoginInfo()),status);
+        }
+        
+    }
+    
+    private void init(RBACController rbac){
+        if(!rbac.isLogin())return;
+        NodeController nc=ControllerFactory.getNodeController();
+        if(ControllerFactory.getUserController().isAdmin(rbac.getLoginInfo())){
+            Iterator<Node> iter=nc.getChilds(DefaultNodeControllerImpl.ROOTNODEID).iterator();
+            while(iter.hasNext()){
+                rootLeaves.add(new NodeTreeLeaf((Node)iter.next()));
+            }
+            checkRepeat();
+        }else{
+            init(ControllerFactory.getRoleUserController().getRoleByUser(rbac.getLoginInfo()));
+        }
+    }
+    
+    private void init(List<Role> roles){
+        List<Node> nodes=new ArrayList<>();
+        RoleNodeController rnc=ControllerFactory.getRoleNodeController();
+        for(Role role:roles){
+            nodes.addAll(rnc.getNodesByRole(role));
+        }
+        init(nodes.toArray(new Node[nodes.size()]));
+        List<NodeTreeLeaf> forDelete=new ArrayList<>();
+        for(NodeTreeLeaf leaf:toList()){
+            boolean exists=false;
+            for(Node node:nodes){
+                if(node.equals(leaf.getEntity())){
+                    exists=true;
+                    break;
+                }
+            }
+            if(!exists)
+                forDelete.add(leaf);
+        }
+        for(NodeTreeLeaf leaf:forDelete)
+            delete(leaf.getEntity().getUuid(), rootLeaves);
+    }
+    
+    private void init(List<Role> roles,boolean status){
+        List<Node> nodes=new ArrayList<>();
+        RoleNodeController rnc=ControllerFactory.getRoleNodeController();
+        for(Role role:roles){
+            if(role.getStatus()!=status)continue;
+            nodes.addAll(rnc.getNodesByRole(role));
+        }
+        init(nodes.toArray(new Node[nodes.size()]), status);
+        List<NodeTreeLeaf> forDelete=new ArrayList<>();
+        for(NodeTreeLeaf leaf:toList()){
+            boolean exists=false;
+            for(Node node:nodes){
+                if(node.getStatus()!=status)
+                    continue;
+                if(node.equals(leaf.getEntity())){
+                    exists=true;
+                    break;
+                }
+            }
+            if(!exists)
+                forDelete.add(leaf);
+        }
+        for(NodeTreeLeaf leaf:forDelete)
+            delete(leaf.getEntity().getUuid(), rootLeaves);
+    }
+    
+    private void init(Node[] nodes){
+        rootLeaves=new ArrayList<>();
+        for(Node node:nodes){
+            rootLeaves.add(new NodeTreeLeaf(node));
+        }
+        checkRepeat();
+    }
+
+    private void init(Node[] nodes,int nodeType){
+        rootLeaves=new ArrayList<>();
+        for(Node node:nodes){
+            if(node.getNodeType()!=nodeType)continue;
+            rootLeaves.add(new NodeTreeLeaf(node, nodeType));
+        }
+        checkRepeat();
+    }
+    
+    private void init(Node[] nodes,boolean status){
+        rootLeaves=new ArrayList<>();
+        for(Node node:nodes){
+            if(node.getStatus()!=status)continue;
+            rootLeaves.add(new NodeTreeLeaf(node,status));
+        }
+        checkRepeat();
+    }
+    
+    private void init(Node[] nodes,boolean status,int nodeType){
+        rootLeaves=new ArrayList<>();
+        for(Node node:nodes){
+            if(node.getStatus()!=status||node.getNodeType()!=nodeType)continue;
+            rootLeaves.add(new NodeTreeLeaf(node, status, nodeType));
+        }
+        checkRepeat();
+    }
+    
+    private void checkRepeat(){
+        List<NodeTreeLeaf> newLeaves=new ArrayList<>();
+        for(int i=0,len=rootLeaves.size();i<len;i++){
+            List<NodeTreeLeaf> leaves=new ArrayList<>();
+            for(int j=0;j<i;j++)
+                leaves.add(rootLeaves.get(j));
+            for(int j=i+1;j<len;j++)
+                leaves.add(rootLeaves.get(j));
+            if(!isExists(rootLeaves.get(i), leaves))
+                newLeaves.add(rootLeaves.get(i));
+        }
+        rootLeaves=newLeaves;
+    }
+    
+    private boolean isExists(NodeTreeLeaf targetLeaf,List<NodeTreeLeaf> leaves){
+        Iterator<NodeTreeLeaf> leafIter=leaves.iterator();
+        while(leafIter.hasNext()){
+            NodeTreeLeaf leaf=leafIter.next();
+            if(targetLeaf.getEntity().equals(leaf.getEntity())||isExists(targetLeaf, leaf.getLeaves()))return true;
+        }
+        return false;
+    }
+    
+    private NodeTreeLeaf find(String nodeId,List<NodeTreeLeaf> leaves){
+        Iterator<NodeTreeLeaf> leafIter=leaves.iterator();
+        while(leafIter.hasNext()){
+            NodeTreeLeaf leaf=leafIter.next();
+            try{
+                if(nodeId.equals(leaf.getEntity().getUuid()))return leaf;
+            }catch(NullPointerException e){
+                continue;
+            }
+            NodeTreeLeaf result=find(nodeId, leaf.getLeaves());
+            if(result!=null)
+                return result;
+        }
+        return null;
+    }
+    
+    public NodeTreeLeaf find(String nodeId){
+        return find(nodeId, rootLeaves);
+    }
+    
+    private boolean delete(String nodeId,List<NodeTreeLeaf> leaves){
+        for(NodeTreeLeaf leaf:leaves){
+            if(leaf.getEntity().getUuid().equals(nodeId)){
+                leaves.remove(leaf);
+                return true;
+            }
+            if(delete(nodeId,leaf.getLeaves()))
+                return true;
+        }
+        return false;
+    }
+    
+    public List<NodeTreeLeaf> getRoots(){
+        return rootLeaves;
+    }
+    
+    private int getDepth(NodeTreeLeaf leaf){
+        if(leaf==null||leaf.getEntity()==null)return 0;
+        int maxDepth=0;
+        Iterator<NodeTreeLeaf> leafIter=leaf.leafIterator();
+        while(leafIter.hasNext()){
+            NodeTreeLeaf childLeaf=leafIter.next();
+            int depth=getDepth(childLeaf);
+            maxDepth=maxDepth<depth ? depth:maxDepth;
+        }
+        return ++maxDepth;
+    }
+    
+    public int getDepth(){
+        if(treeDepth!=Integer.MIN_VALUE)return treeDepth;
+        int maxDepth=0;
+        for(NodeTreeLeaf leaf:rootLeaves){
+            int depth=getDepth(leaf);
+            maxDepth=maxDepth<depth ? depth:maxDepth;
+        }
+        treeDepth=maxDepth!=0 ? maxDepth:treeDepth;
+        return maxDepth;
+    }
+    
+    private List<NodeTreeLeaf> toList(List<NodeTreeLeaf> leaves){
+        List<NodeTreeLeaf> leafList=new ArrayList<>();
+        for(NodeTreeLeaf leaf:leaves){
+            leafList.add(leaf);
+            leafList.addAll(toList(leaf.getLeaves()));
+        }
+        return leafList;
+    }
+    
+    public List<NodeTreeLeaf> toList(){
+        return toList(rootLeaves);
+    }
+    
+    public JSONArray toJSON(){
+        JSONArray arr=new JSONArray();
+        for(NodeTreeLeaf leaf:rootLeaves){
+            try {
+                arr.put(leaf.toJSON());
+            } catch (JSONException ex) {
+                continue;
+            }
+        }
+        return arr;
+    }
+}
---
+++ rbac/src/main/java/dtx/oa/rbac/model/NodeTreeLeaf.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtx.oa.rbac.model;
+
+import dtx.db.ControllerFactory;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author datouxi
+ */
+public class NodeTreeLeaf {
+    
+    private Node entityNode;
+    private List<NodeTreeLeaf> leaves;
+    public final static String NODEID="node-id";
+    public final static String PARENTID="parent-id";
+    public final static String TITLE="title";
+    public final static String SELECTED="selected";
+    public final static String CHILDS="childs";
+    
+    public NodeTreeLeaf(Node node){
+        this.entityNode=node;
+        leaves=new ArrayList<>();
+        List<Node> childs=ControllerFactory.getNodeController().getChilds(node.getUuid());
+        for(Node child:childs){
+            leaves.add(new NodeTreeLeaf(child));
+        }
+    }
+    
+    public NodeTreeLeaf(Node node,int nodeType){
+        this.entityNode=node;
+        leaves=new ArrayList<>();
+        List<Node> childs=ControllerFactory.getNodeController().getChildsByType(node.getUuid(), nodeType);
+        for(Node child:childs){
+            leaves.add(new NodeTreeLeaf(child, nodeType));
+        }
+    }
+    
+    public NodeTreeLeaf(Node node,boolean status){
+        this.entityNode=node;
+        leaves=new ArrayList<>();
+        List<Node> childs=ControllerFactory.getNodeController().getChilds(node.getUuid(), status);
+        for(Node child:childs){
+            leaves.add(new NodeTreeLeaf(child,status));
+        }
+    }
+    
+    public NodeTreeLeaf(Node node,boolean status,int nodeType){
+        this.entityNode=node;
+        leaves=new ArrayList<>();
+        List<Node> childs=ControllerFactory.getNodeController().getChilds(node.getUuid(), status);
+        for(Node child:childs){
+            if(child.getNodeType()==nodeType)
+                leaves.add(new NodeTreeLeaf(child, status, nodeType));
+        }
+    }
+    
+    public Node getEntity(){
+        return entityNode;
+    }
+    
+    public List<NodeTreeLeaf> getLeaves(){
+        return leaves;
+    }
+    
+    public Iterator<NodeTreeLeaf> leafIterator(){
+        return leaves.iterator();
+    }
+    
+    public boolean hasLeaf(){
+        return leaves.size()>0 ? true:false;
+    }
+    
+    public JSONObject toJSON() throws JSONException{
+        JSONObject obj=new JSONObject();
+        obj.put(NODEID, entityNode.getUuid());
+        obj.put(TITLE, entityNode.getTitle());
+        obj.put(PARENTID, entityNode.getParentId());
+        JSONArray childs=new JSONArray();
+        for(NodeTreeLeaf leaf:leaves)
+            childs.put(leaf.toJSON());
+        obj.put(CHILDS, childs);
+        return obj;
+    }
+    
+}
